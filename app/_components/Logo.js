a2e499fce1bca97ea5f1bdbbeeb6a3bc3eb1ed76
Zeydi <zeidiwassa@gmail.com>
2025-12-07 23:44:36 +0100

    chore: initialize project folders and environment

---
+++ app/_components/Logo.js
@@ -0,0 +1,12 @@
+function Logo() {
+  return (
+    <a href='/' className='flex items-center gap-4 z-10'>
+      <img src='./logo.png' height='60' width='60' alt='thrivebooking logo' />
+      <span className='text-xl font-semibold text-primary-100'>
+        ThriveBooking
+      </span>
+    </a>
+  );
+}
+
+export default Logo;
